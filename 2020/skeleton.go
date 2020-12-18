5b1c1b9f7d6a0d547b3114042f7ddd4275866088
Clovis Mugaruka <clovis.mugaruka@gmail.com>
2020-12-18 18:30:49 +0300

    add skeleton (script) used for all solutions

---
+++ 2020/skeleton.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+// command-line flags
+var inputFile = flag.String("inputFile", "input.txt", "The input file to use.")
+var partTwo = flag.Bool("partTwo", false, "Solve part 2?")
+
+func extractData(filename string) ([]string, error) {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(strings.TrimSpace(string(b)), "\n"), nil
+}
+
+// starts programn execution
+func main() {
+	flag.Parse()
+	data, err := extractData(*inputFile)
+	// TODO: add more code, I guess:-)
+}
