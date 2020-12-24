cc9242024c825942b61b29d8af0815cd5ed1f212
Clovis Mugaruka <clovis.mugaruka@gmail.com>
2020-12-24 15:17:11 +0300

    code cleanup

--- 2020/day_07/main.go
+++ 2020/day_07/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
 
@@ -19,45 +20,14 @@ func extractData(filename string) ([]string, error) {
 	return strings.Split(strings.TrimSpace(string(b)), "\n"), nil
 }
 
-func parseLine(line string) (string, []string) {
-	parts := strings.Split(line, " bags contain")
-	destination := parts[0]
-	var sources []string
-	if parts[1] != "no other bags." {
-		sourceSegments := strings.Split(parts[1][:len(parts[1])-1], ",")
-		for _, segment := range sourceSegments {
-			segment = strings.TrimSuffix(segment, "bag")
-			segment = strings.TrimSuffix(segment, "bags")
-			sources = append(sources, strings.SplitN(strings.TrimSpace(segment), " ", 2)[1])
-		}
-	}
-	return destination, sources
-}
-
-// starts program execution
-func main() {
-	flag.Parse()
-	data, err := extractData(*inputFile)
-	if err != nil {
-		panic(err)
-	}
-	graph := make(map[string][]string)
-	for _, line := range data {
-		destination, sources := parseLine(line)
-		for _, source := range sources {
-			graph[source] = append(graph[source], destination)
-		}
-	}
-	// how many bags that can eventually
-	// contain, at least one, 'shiny gold' bag?
+func solvePartOne(key string, parents map[string][]string) {
 	var total int
-	key := "shiny gold"
 	knownKeys := []string{key}
 	found := map[string]bool{key: true}
 	for {
 		nextCycle := make([]string, 0)
 		for _, k := range knownKeys {
-			validParents := graph[k]
+			validParents := parents[k]
 			for _, p := range validParents {
 				if !found[p] {
 					total++
@@ -72,4 +42,40 @@ func main() {
 		knownKeys = nextCycle
 	}
 	fmt.Printf("How many bag can eventually contain at least one shiny gold bag? %d\n", total)
+}
+
+// starts program execution
+func main() {
+	flag.Parse()
+	data, err := extractData(*inputFile)
+	if err != nil {
+		panic(err)
+	}
+	graph := make(map[string]map[string]int)
+	parents := make(map[string][]string)
+	for _, line := range data {
+		parts := strings.Split(line, " bags contain ")
+		parentNode := parts[0]
+		if parts[1] == "no other bags." {
+			continue // empty children nodes
+		}
+		graph[parentNode] = map[string]int{}
+		childrenNodes := strings.Split(parts[1][:len(parts[1])-1], ",")
+		for _, node := range childrenNodes {
+			node = strings.TrimSuffix(node, "bag")
+			node = strings.TrimSuffix(node, "bags")
+			node = strings.TrimSpace(node)
+			nodePart := strings.SplitN(node, " ", 2)
+			value, err := strconv.Atoi(nodePart[0])
+			childNode := nodePart[1]
+			if err != nil {
+				fmt.Printf("Failed to parse %s\n", node)
+				continue
+			}
+			parents[childNode] = append(parents[childNode], parentNode)
+			graph[parentNode][childNode] = value
+		}
+	}
+	//TODO: add part_two solution
+	solvePartOne("shiny gold", parents)
 }
