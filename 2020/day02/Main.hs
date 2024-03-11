30521828bc322ba5b4e15bf4bdfa12d7e36d4971
clovisphere <clovis.mugaruka@gmail.com>
2024-03-11 15:54:09 +0300

    Add Day 02 solution in Haskell

---
+++ 2020/day02/Main.hs
@@ -0,0 +1,35 @@
+module Main where
+
+data Row = Row { rowChar :: Char
+               , rowRange:: (Int, Int)
+               , rowPassword :: String
+               } deriving Show
+
+tail' :: [a] -> [a]
+tail' [] = []
+tail' (_:xs) = xs
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn _ [] = []
+splitOn d s = x:splitOn d (tail' s')
+    where (x, s') = span (/= d) s
+
+rowFromString :: String -> Row
+rowFromString s = Row c (read low, read high) password
+    where [policy, ' ':password] = splitOn ':' s
+          [range, c:_] = splitOn ' ' policy
+          [low, high] = splitOn '-' range
+
+-- part 01
+isCorrectV1 :: Row -> Bool
+isCorrectV1 (Row c (low, high) password) = low <= n && n <= high
+    where n = length $ filter (== c) password
+
+-- part 02
+isCorrectV2 :: Row -> Bool
+isCorrectV2 (Row c (low, high) password) = (a == c) /= (b == c)
+    where a = password !! (low - 1)
+          b = password !! (high - 1)
+
+main :: IO ()
+main = interact $ (++ "\n") . show . length . filter isCorrectV2 . map rowFromString . lines
