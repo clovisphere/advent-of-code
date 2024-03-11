79231ef5e5f893017a02d4e938ec2117d285d5f5
clovisphere <clovis.mugaruka@gmail.com>
2024-03-11 18:36:37 +0300

    Add Day 03 (part 02) solution in Java

--- 2020/day03/Main.java
+++ 2020/day03/Main.java
@@ -6,16 +6,27 @@ import java.nio.file.Path;
 public class Main {
     public static void main(String... args) throws IOException {
         // read content of file, and split on '\n' 😽
-        String[] input = Files.readString(Path.of("input.txt")).split("\n");
-        int height = input.length, width = input[0].length();
+        var input = Files.readString(Path.of("input.txt")).split("\n");
 
-        System.out.printf("part 01: %d\n", part01(input, height, width));
+        // part 01
+        System.out.printf("part 01: %d\n", countTrees(input, 3, 1));
+
+        // part 02
+        int[][] slopes = {
+            {1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}
+        };
+
+        long result = 1L;
+        for(var slope : slopes) {
+            result *= countTrees(input, slope[0], slope[1]);
+        }
+        System.out.printf("part 02: %d\n", result);
     }
 
-    private static int part01(String[] input, int height, int width) {
-        int x = 0, y = 0, result = 0;
-        while(++y < height) {
-            if (input[y].charAt((x += 3) % width) == '#') {
+    private static int countTrees(String[] input, int dx, int dy) {
+        int x = 0, y = 0, result = 0, height = input.length, width = input[0].length();
+        while((y += dy) < height) {
+            if (input[y].charAt((x += dx) % width) == '#') {
                 result++;
             }
         }
